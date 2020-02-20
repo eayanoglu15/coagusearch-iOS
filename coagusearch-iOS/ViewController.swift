5597afb9ea2351aa6705fcfd96f158e33c551d7d
Unknown <eayanoglu15@ku.edu.tr>
2020-02-20 16:38:57 +0300

    Initial Empty XCode Project

---
+++ coagusearch-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coagusearch-iOS
+//
+//  Created by Ege Melis Ayanoğlu on 20.02.2020.
+//  Copyright © 2020 coagusearch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
